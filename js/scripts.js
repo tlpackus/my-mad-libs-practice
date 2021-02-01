03da72af31103e3c69ee225608f5c05d325c46fd
tlpackus <tlpackus@gmail.com>
2021-01-31 16:21:22 -0800

    now added a loop and array to conciecedize text

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    const inputs = [$("input#person1").val(),$("input#person2").val(),$("input#animal").val(),$("input#exclamation").val(),$("input#verb").val(),$("input#noun").val()]
+
+    const classes = [".person1", ".person2", ".animal", ".exclamation", ".verb", ".noun"]
+
+    let iterator = 0;
+    inputs.forEach(function(element) {
+      $(classes[iterator]).text(element);
+      iterator += 1;
+    });
+    // const person1Input = $("input#person1").val();
+    // const person2Input = $("input#person2").val();
+    // const animalInput= $("input#animal").val();
+    // const exclamationInput = $("input#exclamation").val();
+    // const verbInput = $("input#verb").val();
+    // const nounInput = $("input#noun").val();
+
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
 
     $("#story").show();
 
